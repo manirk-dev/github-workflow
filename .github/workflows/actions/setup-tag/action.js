6664b7db89ec83dcf427ed1dc23d86ea269502e0
Manikandan Ravikumar <manirk.dev@gmail.com>
2023-09-30 20:01:43 +0300

    Persist after build completion

--- .github/workflows/actions/setup-tag/action.js
+++ .github/workflows/actions/setup-tag/action.js
@@ -1,23 +1,14 @@
 const core = require("@actions/core")
-const github = require("@actions/github")
 
 try {
 
     const tag = core.getInput("tag")
-    const token = core.getInput("token")
+
     const target = core.getInput("target")
 
     const sanitized = Number(tag.split(".").join("")) + 1
 
     const converted = sanitized.toString().padStart(3, "0").split("").join(".")
-
-    const octokit = github.getOctokit(token)
-
-    octokit.request("PATCH /repos/{owner}/{repo}/actions/variables/{name}", {
-        ...github.context.repo,
-        name: "TAG_VERSION",
-        value: converted
-    })
 
     const prefixed = `${target.charAt(0)}${converted}`
 
