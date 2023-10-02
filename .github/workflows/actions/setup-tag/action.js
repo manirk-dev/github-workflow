6129ee4abea6840e9c9bda6d3e531a113b22812c
Manikandan Ravikumar <manirk.dev@gmail.com>
2023-10-02 13:00:50 +0300

    simplify tag handling

--- .github/workflows/actions/setup-tag/action.js
+++ .github/workflows/actions/setup-tag/action.js
@@ -4,13 +4,18 @@ try {
 
     const tag = core.getInput("tag")
 
-    const target = core.getInput("target")
+    const tagComponents = tag.split(/([a-z]+)/)
+        .filter(item => item.trim().length > 0)
 
-    const sanitized = Number(tag.split(".").join("")) + 1
+    const target = tagComponents[0]
+
+    const version = tagComponents[1]
+
+    const sanitized = Number(version.split(".").join("")) + 1
 
     const converted = sanitized.toString().padStart(3, "0").split("").join(".")
 
-    const prefixed = `${target.charAt(0)}${converted}`
+    const prefixed = `${target}${converted}`
 
     core.setOutput("tag-version", prefixed)
 
